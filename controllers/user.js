5bbce0c19f1249da19c3c343dab025bf2a8c3a08
ozzkan-burak <brk.ozzkan@gmail.com>
2024-03-17 17:26:44 +0300

    episode6

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,26 @@
 const User = require("../models/user");
 
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
   console.log("req.body", req.body);
   const user = new User(req.body);
-  user.save((err, user) => {
-    if (err) {
-      return res.status(400).json({
-        error,
-      });
-    }
+  // user.save((err, user) => {
+  //   if (err) {
+  //     return res.status(400).json({
+  //       error,
+  //     });
+  //   }
+  //   res.json({
+  //     user,
+  //   });
+  // });
+
+  try {
+    await user.save();
+    console.log("User created successful");
     res.json({
       user,
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
